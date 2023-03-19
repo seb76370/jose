eb5ab590303dbe37c14be6c4e118999ad15f77e7
sebastien baudet <seb.baudet76@gmail.com>
2023-03-19 10:24:21 +0100

    change title

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -9,7 +9,7 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
-  title = "Jose efferreddf"
+  title = "Jose effedzdzdzrreddf"
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
